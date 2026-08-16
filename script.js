90e6e7999836fc87cce5eea10bf01580f722257f
dennimontenegro5674-jpg <dennimontenegro5674@gmail.com>
2026-08-15 22:33:01 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function abrirLogin() {
+    document.getElementById("login").style.display = "flex";
+}
+
+function cerrarLogin() {
+    document.getElementById("login").style.display = "none";
+}
+
+function iniciarSesion() {
+    alert("¡Bienvenido a Mi Universo! 💗✨");
+}
